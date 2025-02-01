f61e019caf4c057bfc908be6a02aac002f3eebfe
Camille CYL-B <benhammadcamille@hotmail.fr>
2025-02-01 23:26:44 +0100

    feat(app.js): added router logic

--- big-three-front/src/App.jsx
+++ big-three-front/src/App.jsx
@@ -1,17 +1,28 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
-// import { ChoosePlayerPage } from "./pages/choosePlayerPage.jsx";
+import { ChoosePlayerPage } from "./pages/choosePlayerPage.jsx";
 import TennisCourtPage from "./pages/tennisCourtPage.jsx";
+import { ChoosePlayerLayout } from "./layouts/choosePlayerLayout.jsx";
+import { PlayerForm } from "./components/playerForm.jsx";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={new QueryClient()}>
-      
-      <TennisCourtPage/>
-       
-      </QueryClientProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={new QueryClient()}>
+          <Routes>
+            <Route path="/select" >
+              <Route index element={<ChoosePlayerPage />} />
+              <Route element={<ChoosePlayerLayout />}>
+                <Route path=":id" element={<PlayerForm />} />
+              </Route>
+            </Route>
+            <Route path="/tennis-court" element={<TennisCourtPage />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
     </>
   );
 }
